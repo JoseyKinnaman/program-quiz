b212a070493054f342ca18474466f71e2d58ec81
Jozy Kinnaman <jozypants@gmail.com>
2020-02-25 13:08:36 -0800

    add variables to js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
- 
+  var goals = $("#goals").val();
+  var pref = $("#pref").val();
+  var importance = $("#import").val();
+  var age = $("#age").val;
+  var favoriteColor = $("#color").val();
+  
 
 
 event.preventDefault();
