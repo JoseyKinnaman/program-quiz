265729f113f4d4419fea29ce19253472aa659b4c
Jozy Kinnaman <jozypants@gmail.com>
2020-02-25 16:26:24 -0800

    add funcionality to js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,40 @@
 $(document).ready(function() {
-  var goals = $("#goals").val();
-  var pref = $("#pref").val();
-  var importance = $("#import").val();
-  var age = $("#age").val;
-  var favoriteColor = $("#color").val();
-  
+  $("form#program").submit(function(event) {
+    var goals = parseInt($("#goals").val());
+    var pref = parseInt($("#pref").val());
+    var importance = parseInt($("#import").val());
+    var age = parseInt($("#age").val());
+    var favoriteColor = $("#color").val();
+    event.preventDefault();
 
+    console.log(goals);
+    console.log(pref);
+    console.log(importance);
+    console.log(age);
+    console.log(favoriteColor);
 
-event.preventDefault();
+    if (goals === "1" && age <= "50") {
+      $(".java").show();
+    }
+    //   $(".react").hide();
+    //   $(".ruby").hide();
+    //   $(".c-sharp").hide();
+    //   debugger;
+    // } else if (pref === "1") {
+    //   $(".react").show();
+    //   $(".java").hide();
+    //   $(".ruby").hide();
+    //   $(".c-sharp").hide();
+    // } else if (importance === "1") {
+    //   $(".ruby").show();
+    //   $(".react").hide();
+    //   $(".java").hide();
+    //   $(".c-sharp").hide();
+    // } else if (goals === "3" || pref === "2") {
+    //   $(".c-sharp").show();
+    //   $(".react").hide();
+    //   $(".ruby").hide();
+    //   $(".java").hide();
+    // }
   });
 });
